a510a4169c7bee12ddab89203ba220b715944bee
Mike Boers <mboers@keypics.com>
2013-01-18 10:09:16 -0800

    Setup logging

--- __init__.py
+++ __init__.py
@@ -21,7 +21,6 @@ error). This was fixed in Python2.7, but we don't have that luxury.
 
 """
 
-
 import errno
 import os
 import warnings
@@ -29,79 +28,82 @@ import warnings
 
 # We need to be super careful in this module.
 try:
-    from .environ import apply_diff as environ_apply_diff
+    from .logging import setup_logging
 except ImportError, why:
-    warnings.warn('Error while importing sitecustomize.environ: %r' % why)
-    def environ_apply_diff(*args, **kwargs):
-        pass
+    warnings.warn('Error while importing sitecustomize.logging: %r' % why)
+else:
+    try:
+        setup_logging()
+    except Exception, why:
+        warnings.warn('Error while setting up logging: %r' % why)
+
+
+# Setup the pseudo site-packages.
 try:
     from .sites import add_site_dir
 except ImportError, why:
     warnings.warn('Error while importing sitecustomize.sites: %r' % why)
-    def add_site_dir(*args, **kwargs):
-        pass
-try:
-    from .monkeypatch import patch
-except ImportError, why:
-    warnings.warn('Error while importing sitecustomize.monkeypatch: %r' % why)
-    def patch(*args, **kwargs):
-        return lambda *a, **kw: None
+else:
 
+    # Where do we want to start inserting directories into sys.path? Just before
+    # this module, of course. So determine where we were imported from.
+    our_sys_path = os.path.abspath(os.path.join(__file__,
+        os.path.pardir,
+        os.path.pardir,
+    ))
 
-__all__ = ['add_site_dir', 'patch']
-
-
-# Where do we want to start inserting directories into sys.path? Just before
-# this module, of course. So determine where we were imported from.
-our_sys_path = os.path.abspath(os.path.join(__file__,
-    os.path.pardir,
-    os.path.pardir,
-))
-
-
-# Setup the pseudo site-packages.
-sites = [x.strip() for x in os.environ.get('KS_PYTHON_SITES', '').split(':') if x]
-sites.append(our_sys_path)
-for site in sites:
-    try:
-        add_site_dir(site, before=our_sys_path)
-    except Exception, why:
-        warnings.warn('Error while adding site-package %r: %r' % (site, why))
+    sites = [x.strip() for x in os.environ.get('KS_PYTHON_SITES', '').split(':') if x]
+    sites.append(our_sys_path)
+    for site in sites:
+        try:
+            add_site_dir(site, before=our_sys_path)
+        except Exception, why:
+            warnings.warn('Error while adding site-package %r: %r' % (site, why))
 
 
 # Restore frozen environment variables.
 try:
-    environ_apply_diff()
+    from .environ import apply_diff as environ_apply_diff
 except ImportError, why:
-    warnings.warn('Error while running sitecustomize.environ.apply_diff: %r' % why)
-
-
-# Monkey-patch chflags for Python2.6 since our NFS does not support it and
-# Python2.6 does not ignore that lack of support.
-# See: http://hg.python.org/cpython/rev/e12efebc3ba6/
-@patch(os, 'chflags', max_version=(2, 6))
-def os_chflags(func, *args, **kwargs):
-    """Monkey-patched to ignore "Not Supported" errors for our NFS."""
-    
-    # Some OSes don't have the function, so screw it.
-    if not func:
-        return
-    
+    warnings.warn('Error while importing sitecustomize.environ: %r' % why)
+else:
     try:
-        return func(*args, **kwargs)
-    except OSError, why:
+        environ_apply_diff()
+    except ImportError, why:
+        warnings.warn('Error while running sitecustomize.environ.apply_diff: %r' % why)
+
+
+try:
+    from .monkeypatch import patch
+except ImportError, why:
+    warnings.warn('Error while importing sitecustomize.monkeypatch: %r' % why)
+else:
+    # Monkey-patch chflags for Python2.6 since our NFS does not support it and
+    # Python2.6 does not ignore that lack of support.
+    # See: http://hg.python.org/cpython/rev/e12efebc3ba6/
+    @patch(os, 'chflags', max_version=(2, 6))
+    def os_chflags(func, *args, **kwargs):
+        """Monkey-patched to ignore "Not Supported" errors for our NFS."""
         
-        # Ignore "Not Supported" errors.
-        for err in 'EOPNOTSUPP', 'ENOTSUP':
-            if hasattr(errno, err) and why.errno == getattr(errno, err):
-                return
-        
-        # Must hardcode the errno because my version has no constant.
-        if why.errno == 45:
+        # Some OSes don't have the function, so screw it.
+        if not func:
             return
         
-        # This must be an important error.
-        raise
+        try:
+            return func(*args, **kwargs)
+        except OSError, why:
+            
+            # Ignore "Not Supported" errors.
+            for err in 'EOPNOTSUPP', 'ENOTSUP':
+                if hasattr(errno, err) and why.errno == getattr(errno, err):
+                    return
+            
+            # Must hardcode the errno because my version has no constant.
+            if why.errno == 45:
+                return
+            
+            # This must be an important error.
+            raise
 
 
 
---
+++ logging.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+
+import logging
+import os
+import re
+import sys
+
+
+log = logging.getLogger(__name__)
+
+
+def setup_logging():
+
+    level = {
+        '1': logging.INFO,
+        '2': logging.DEBUG,
+    }.get(os.environ.get('KS_VERBOSE'))
+
+    logging.basicConfig(
+        format='%(asctime)-15s %(levelname)s %(name)s: %(message)s',
+        level=level,
+        stream=sys.stderr,
+    )
+
+    requested_levels = os.environ.get('KS_LOG_LEVELS')
+    if requested_levels:
+
+        requested_levels = [x.strip() for x in re.split(r'[\s,]+', requested_levels)]
+        requested_levels = [x for x in requested_levels if x]
+
+        for spec in requested_levels:
+
+            parts = spec.split(':')
+            if len(parts) != 2:
+                log.error('%r is invalid log specification' % spec)
+                continue
+
+            name, level = parts
+            name = name.strip() or None
+            try:
+                level = int(level)
+            except ValueError:
+                level = getattr(logging, level.upper(), None)
+            if not isinstance(level, int):
+                log.error('%r is invalid log level')
+                continue
+
+            logger = logging.getLogger(name)
+            logger.setLevel(level)
+            log.debug('%s set to %s' % (name, level))
