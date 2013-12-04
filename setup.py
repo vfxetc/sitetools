10b1de65735c3b5bf4512889172c41427a16344b
Mike Boers <github@mikeboers.com>
2013-12-04 14:45:58 -0800

    Turn into an installable package

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+setup(
+    
+    name='sitecustomize',
+    version='0.1.0',
+    description='Customize your Python startup sequence.',
+    url='http://github.com/mikeboers/sitecustomize',
+    
+    packages=['sitecustomize'],
+    
+    author='Mike Boers',
+    author_email='sitecustomize@mikeboers.com',
+    license='BSD-3',
+
+)
