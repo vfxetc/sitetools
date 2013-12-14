bc571acbc97bd01940aae3c6739fa052985531d9
Mike Boers <github@mikeboers.com>
2013-12-13 20:38:55 -0800

    Bash completions for dev wrapper

---
+++ bashrc.sh
@@ -0,0 +1,18 @@
+# Dev is a wrapper, so add completions.
+# `dev [-u <user>] [<command> [<args>]]`
+function _dev_completions {
+    
+    local cword pword
+    cword="${COMP_WORDS[COMP_CWORD]}"
+    pword="${COMP_WORDS[$((COMP_CWORD - 1))]}"
+    
+    case "$pword" in
+    
+        -u | --user )
+            COMPREPLY=$(compgen -u -- "$cword")
+            ;;
+        
+    esac
+}
+complete -cf -F _dev_completions dev
+
