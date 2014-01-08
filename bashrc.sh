db8d14ee40a4586f01706fe6f76bd9afb8478200
Mike Boers <github@mikeboers.com>
2014-01-08 13:18:08 -0800

    dev-deactivate to get you back to "normal"

--- bashrc.sh
+++ bashrc.sh
@@ -18,6 +18,24 @@ complete -cf -F _dev_completions dev
 
 
 function dev-shell {
+    # Save all of the KS variables so that we can restore them.
+    export _OLD_KS_ENV="$(env | grep '^KS_')"
     eval "$(dev --export "$@")"
+}
+
+function dev-deactivate {
+    if [[ -n "$_OLD_KS_ENV" ]]; then
+
+        # Wipe out the current KS variables.
+        local name
+        for name in $(env | grep '^KS_' | cut -d= -f1); do
+            unset $name
+        done
+
+        # Restore!
+        eval "$_OLD_KS_ENV"
+        unset _OLD_KS_ENV
+
+    fi
 }
 
